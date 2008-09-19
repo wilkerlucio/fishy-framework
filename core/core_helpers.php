d3e46859ef7d6d115406121a9fabe6fcd1bec0dc
wilkerlucio <wilkerlucio@088b31fc-84f3-11dd-8f23-d9c8888c2974>
2008-09-19 21:06:09 +0000

    some changes
    git-svn-id: http://fishyframework.googlecode.com/svn/trunk@4 088b31fc-84f3-11dd-8f23-d9c8888c2974

--- core/core_helpers.php
+++ core/core_helpers.php
@@ -1,39 +1,6 @@
 <?php
 
 /**
- * This function get the base URL of site
- *
- * @param $sulfix Sulfix to be append at end of file
- * @return string The final path
- */
-function base_url($sulfix = '')
-{
-	return FISHY_BASE_URL . $sulfix;
-}
-
-/**
- * This function get one internal url of site
- *
- * @param $sulfix Sulfix to be append at end of file
- * @return string The final path
- */
-function site_url($sulfix = '')
-{
-	return FISHY_BASE_URL . FISHY_INDEX_PAGE . '/' . $sulfix;
-}
-
-/**
- * This function get one public url of site
- *
- * @param $sulfix Sulfix to be append at end of file
- * @return string The final path
- */
-function public_url($sulfix = '')
-{
-	return FISHY_PUBLIC_PATH . '/' . $sulfix;
-}
-
-/**
  * Simple class that doesn't anything
  */
 class BlankObject
