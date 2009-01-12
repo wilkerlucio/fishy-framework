57ab8e07f40599af4cac030ccb426a9d8ff52cf0
Wilker Lucio <wilkerlucio@gmail.com>
2009-01-12 16:26:42 -0300

    modified route way and created base spec

--- libraries/Router.php
+++ libraries/Router.php
@@ -18,15 +18,151 @@
 
 class Fishy_Router
 {
-	private $config;
 	private $routes;
 	
-	public function __construct($config = array())
+	public function __construct()
 	{
-		$this->config = $config;
-		$this->routes = isset($config['routes']) ? $config['routes'] : array();
+		$this->routes = array();
 		
-		$this->routes[] = array('(.*)', '$1');
+		$this->setup();
+	}
+	
+	protected function setup() {}
+	
+	public function match($path)
+	{
+		$info = array(
+			'controller' => null,
+			'action' => null,
+			'format' => 'html',
+			'params' => array()
+		);
+		
+		$found = false;
+		
+		foreach ($this->routes as $route) {
+			$matches = array();
+			$data = array();
+			
+			if (preg_match($route['match_pattern'], $path, $matches)) {
+				foreach ($route['names'] as $key => $name) {
+					$data[$name] = $matches[$key + 1];
+				}
+				
+				$found = true;
+				
+				break;
+			}
+		}
+		
+		if (!$found) {
+			throw new Exception('Route not found');
+		}
+		
+		foreach ($data as $key => $value) {
+			if (in_array($key, array('controller', 'action', 'format'))) {
+				$info[$key] = $value;
+			} else {
+				$info['params'][$key] = $value;
+			}
+		}
+		
+		return $info;
+	}
+	
+	private function escape_pattern_char($char)
+	{
+		$to_escape = './\\()[]?*+';
+		
+		if (strpos($to_escape, $char) !== false) {
+			$char = '\\' . $char;
+		}
+		
+		return $char;
+	}
+	
+	public function default_action()
+	{
+		return 'index';
+	}
+	
+	public function map_connect($pattern, $options = array())
+	{
+		$options = array_merge($this->default_options(), $options);
+		
+		$extract_match = $this->create_match_pattern($pattern);
+		
+		$route = array();
+		$route['pattern'] = $pattern;
+		$route['match_pattern'] = $extract_match['pattern'];
+		$route['names'] = $extract_match['names'];
+		$route['name'] = $options['name'];
+		$route['options'] = $options;
+		
+		$this->routes[] = $route;
+	}
+	
+	public function create_match_pattern($pattern)
+	{
+		$var_buffer = '';
+		$cur = 0;
+		$max = strlen($pattern);
+		$state = 0;
+		$match_pattern = '/^';
+		$names = array();
+		
+		while ($cur < $max) {
+			$char = $pattern[$cur];
+			
+			if ($state == 0) {
+				if ($char == ':') {
+					$var_buffer = '';
+					$state = 1;
+				} else {
+					$match_pattern .= $this->escape_pattern_char($char);
+				}
+			} elseif ($state == 1) {
+				$code = ord($char);
+				
+				if ($char == '_' || ($code > 96 && $code < 123)) {
+					$var_buffer .= $char;
+				} else {
+					$match_pattern .= '([a-z0-9_-]+?)';
+					$match_pattern .= $this->escape_pattern_char($char);
+					$state = 0;
+					$names[] = $var_buffer;
+					$var_buffer = '';
+				}
+			}
+			
+			$cur++;
+		}
+		
+		if ($state == 1) {
+			$match_pattern .= '([a-z0-9_-]+?)';
+			$names[] = $var_buffer;
+		}
+		
+		$match_pattern .= '$/i';
+		
+		return array("pattern" => $match_pattern, "names" => $names);
+	}
+	
+	public function __call($method, $args)
+	{
+		//TODO: implement a easy way to created named routes
+	}
+	
+	private function default_options()
+	{
+		return array(
+			'controller' => null,
+			'action' => null,
+			'name' => null,
+			'conditions' => array(),
+			'defaults' => array(),
+			'format' => 'html'
+		);
 	}
 	
 	public function parse($path)
---
+++ spec/libraries/RouterSpec.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Copyright 2009 Wilker Lucio <wilkerlucio@gmail.com>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+require_once dirname(__FILE__) . '/../../libraries/Router.php';
+
+class RouteSpec extends Fishy_Router
+{
+	protected function setup()
+	{
+		$this->map_connect(':controller/:action/:id');
+		$this->map_connect(':controller/:action/:id.:format');
+	}
+}
+
+class DescribeRouter extends PHPSpec_Context
+{
+	public function itShouldGenerateAPregPatternFromBasePattern()
+	{
+		$router = new RouteSpec();
+		
+		$result = $router->create_match_pattern(':controller/:action/:id');
+		
+		$this->spec($result['pattern'])->should->be('/^([a-z0-9_-]+?)\/([a-z0-9_-]+?)\/([a-z0-9_-]+?)$/i');
+		$this->spec($result['names'])->should->be(array('controller', 'action', 'id'));
+	}
+	
+	public function itShouldMatchRouteDefinition()
+	{
+		$router = new RouteSpec();
+		
+		$info = $router->match('main/view/1');
+		
+		$this->spec($info['controller'])->should->be('main');
+		$this->spec($info['action'])->should->be('view');
+		$this->spec($info['format'])->should->be('html');
+		
+		$info = $router->match('main/view/1.js');
+		
+		$this->spec($info['controller'])->should->be('main');
+		$this->spec($info['action'])->should->be('view');
+		$this->spec($info['format'])->should->be('js');
+	}
+}
